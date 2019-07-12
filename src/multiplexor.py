308cb8fbb1b00f5ea7a533d3b81a21e46bf5bc2e
Guido Rombolá <guidorombola@gmail.com>
2019-07-12 10:20:42 -0300

    Define multiplexor con metodos para estandarizar longitudes y multiplexar

---
+++ src/multiplexor.py
@@ -0,0 +1,45 @@
+import math
+
+
+class Multiplexor:
+
+    def __init__(self, fs, tiempo_por_canal, canales):
+        self.fs = fs
+        self.tiempo_por_canal = tiempo_por_canal
+        self.canales = canales
+
+    def multiplexar(self):
+        senial_multiplexada = []
+        muestras_por_canal = self.fs * self.tiempo_por_canal
+        canales_estandarizados = self.igualar_longitudes(muestras_por_canal)
+        muestras_multiplexadas = 0
+        while (len(canales_estandarizados[0]) - muestras_multiplexadas) > 0:
+            trama = []
+            for j in range(len(canales_estandarizados)):
+                slot_canal = []
+                slot_canal.append(canales_estandarizados[j][muestras_multiplexadas: muestras_multiplexadas + muestras_por_canal])
+                trama.extend(slot_canal)
+
+            muestras_multiplexadas += muestras_por_canal
+            senial_multiplexada.append(trama)
+
+        return senial_multiplexada
+
+    def igualar_longitudes(self, muestras_por_canal):
+        senial_mas_larga = max([len(canal) for canal in self.canales])
+        a = math.ceil(senial_mas_larga / muestras_por_canal)
+        b = a * muestras_por_canal
+        canales_estandarizados = []
+        for canal in self.canales:
+            canal_estandarizado = []
+            vacios = [None] * (b - len(canal))
+            canal_estandarizado.extend(canal)
+            canal_estandarizado.extend(vacios)
+            canales_estandarizados.append(canal_estandarizado)
+        return canales_estandarizados
+
+
+if __name__ == '__main__':
+    canales = [[1, 2, 3, 4, 9, 10, 11], [5, 6, 7, 8]]
+    mul = Multiplexor(3, 1, canales)
+    print(mul.multiplexar())
